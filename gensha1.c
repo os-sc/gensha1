76816be7b6d73b7c23e0f257f65f6d67753ee3df
os <gh@svis.ch>
2018-03-12 21:36:43 +0100

    Added error messages

--- gensha1.c
+++ gensha1.c
@@ -31,6 +31,11 @@ uint32_t maj(uint32_t x, uint32_t y, uint32_t z)
 }
 
 void set_constants(uint32_t* arr) {
+    if (sizeof(arr) != 80 * sizeof(uint32_t)){
+        printf("[ERROR] Inavlid constants array size.\n");
+        exit(EINVAL);
+    }
+
     for(size_t i = 0; i < 80; i++) {
         if (i >=  0 && i <= 19) arr[i] = 0x5a827999;
         if (i >= 20 && i <= 39) arr[i] = 0x6ed9eba1;
@@ -45,8 +50,9 @@ uint32_t f(uint32_t t, uint32_t x, uint32_t y, uint32_t z)
     if (t >= 20 && t <= 39) return parity(x, y, z);
     if (t >= 40 && t <= 59) return maj(x, y, z);
     if (t >= 60 && t <= 79) return parity(x, y, z);
-    printf("no %d\n", t);
-    exit(ENOSYS);
+
+    printf("[ERROR] Inavlid constants array.\n");
+    exit(EINVAL);
 }
 
 int main(int argc, char **argv)
